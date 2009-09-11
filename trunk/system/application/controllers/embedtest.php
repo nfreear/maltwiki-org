d910c375742c18b4c8df0604c53837f8dec82df3
Nick Freear <nfreear@yahoo.co.uk>
2009-09-11 16:13:27 +0000

    incubator/maltwiki_org: Added oembed_hook.php, jquery.oembed.js etc. - as emailed to Juliette 6/9/09.
    git-svn-id: https://wahoo.open.ac.uk/svn/repos/incubator/maltwiki_org@3445 931b2e54-a526-0410-9ac3-d7400825cc31

---
+++ trunk/system/application/controllers/embedtest.php
@@ -0,0 +1,15 @@
+<?php
+#ou-specific.
+
+class EmbedTest extends Controller {
+
+	function index()
+	{
+		$this->load->view('embedtestview');
+	}
+
+  function test($a, $b)
+	{
+		echo 'An embed test [embed:http://youtube.com/watch?v=grqt3HoLOIA] '.$a;
+	}
+}
---
+++ trunk/system/application/views/embedtestview.php
@@ -0,0 +1,28 @@
+<?php
+#ou-specific.
+?><!DOCTYPE html><html lang="en"><head><meta charset=utf-8 /><title>oEmbed hook test</title>
+</head><body>
+<h1>oEmbed hook test</h1>
+
+	<h2>Youtube</h2>
+
+	<p>[embed: http://youtube.com/watch?v=grqt3HoLOIA | Learn about Moodle]</p>
+
+	<a rel="embed" href="http://youtube.com/watch?v=grqt3HoLOIA">Learn about Moodle</a>
+
+
+	<h2>Slideshare</h2>
+	
+	<p>[embed: http://slideshare.net/nfreear/multimedia-accessibilitycalrg-09 | Multimedia Accessibility - CALRG'09]</p>
+	
+	<a rel="embed" href="http://www.slideshare.net/grainne/cloudworks-overview-1074075">Multimedia Accessibility - CALRG'09</a>
+
+
+  <h2>Another link, and an error</h2>
+
+  <a href="http://cloudworks.ac.uk/">Cloudworks</a>
+
+  <p>[embed: http://Whotube.com/watch?w=grqt3HoLOIA | Learn something]</p>
+
+
+</body></html>
